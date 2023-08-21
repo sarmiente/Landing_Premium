75358345257c39498d5b17abc864931bd42ce1bd
sarmiente <53012993+sarmiente@users.noreply.github.com>
2023-08-21 13:50:21 -0500

    1 file modified, 1 added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Contador from '../Components/Contador';
 import Image from 'next/image';
 import Logo from '../public/assets/premiumdrop.svg';
+import Flecha from '../public/assets/flecha.svg';
 
 export default function Home() {
   return (
@@ -13,7 +14,9 @@ export default function Home() {
       <div className="text-center p-3 bg-blue-500 text-white text-xl lg:text-3xl">
         <Contador />
       </div>
-
+      <h1 className="font-semibold text-xl lg:text-3xl text-center">
+        Accede al curso
+      </h1>
       <div className="container mx-auto flex justify-center pt-5">
         <Image width="250" src={Logo} />
       </div>
@@ -25,41 +28,35 @@ export default function Home() {
             Genera ingresos a través de una comisión por cada venta, y ten la
             libertad de establecer los precios de los productos.
           </p>
-
-          {/* 
-         <h1 className="p-4">
-            CREA UN EMPRENDIMIENTO VIRTUAL Y EMPIEZA A VENDER PRODUCTOS
-            DIGITALES
-          </h1>
-
-          <p className="p-4">MONETIZA TU TALENTO Y GANA EL 100% DE TU CURSO</p>
-
-          <p className="">
-            O AFILIATE AL PRODUCTO DIGITAL DE UN TERCERO Y GANA UNA COMISION
-            ENTRE EL 60 Y 80%
-          </p>
-         */}
         </main>
       </div>
-      <div className="container mx-auto bg-blue-500 p-10 font-semibold text-xl lg:text-3xl text-center">
-        <h2 className="mb-2">
+      <div className="container mx-auto bg-blue-500 p-10 font-semibold text-xl lg:text-3xl text-center text-white">
+        <h2>
           CREA UN EMPRENDIMIENTO VIRTUAL Y EMPIEZA A VENDER PRODUCTOS DIGITALES
         </h2>
+        <br />
         <p>En este curso aprenderas a encontrar y vender productos premium.</p>
       </div>
 
-      <div className="container mx-auto p-10">
-        <h2>Productos Exclusivos</h2>
+      <div className="container mx-auto p-10 font-semibold text-xl lg:text-3xl text-center">
+        <h2>Agrega Productos Exclusivos a tu tienda</h2>
         <p>Te enseñaremos a encontrar y vender productos premium.</p>
       </div>
 
+      <div className="container mx-auto flex justify-center">
+        <Image width="250" src={Flecha} />
+      </div>
+
       <div className="container mx-auto bg-black text-white text-center p-20">
-        <button className="bg-red-500 p-4 rounded-xl">
+        <button className="hover:scale-105 bg-red-500 p-4 rounded-xl" src="">
           ¡SI, QUIERO Acceder Ahora!
         </button>
       </div>
+      <div className="container mx-auto flex justify-center p-10">
+        <Image width="250" src={Logo} />
+      </div>
 
-      <footer className="flex items-center justify-center h-10 bg-gray-800 text-white ">
+      <footer className="container mx-auto flex justify-center h-auto bg-gray-800 text-white p-10">
         <p>© {new Date().getFullYear()} Dropshipping Premium</p>
       </footer>
     </div>
