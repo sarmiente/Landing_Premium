fa288b55d72198ffaace7a20146d0d8b0047a079
sarmiente <53012993+sarmiente@users.noreply.github.com>
2023-08-21 20:36:10 -0500

    1 file modified, 1 deleted

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         Accede al curso ahora
       </h1>
       <div className="container mx-auto flex justify-center pt-5">
-        <Image width="250" src={Logo2} />
+        <Image width="350" src={Logo2} />
       </div>
 
       <div className="container mx-auto p-5 lg:p-10">
@@ -32,6 +32,17 @@ export default function Home() {
             libertad de establecer los precios de los productos.
           </p>
         </main>
+      </div>
+
+      <div className="container mx-auto text-center mb-5 ">
+        <a
+          href="https://pay.hotmart.com/F37843021Y"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="animate-bounce hover:scale-105 rounded-xl font-bold bg-black text-white p-3 rounded-md mt-4 inline-block"
+        >
+          ¡Acceder Ahora!
+        </a>
       </div>
 
       <div className="container mx-auto bg-blue-500 p-10 font-semibold text-xl lg:text-3xl text-center text-white">
